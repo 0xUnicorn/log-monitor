2d8d8b95f20e7eb59492fb10e36e7aaec865a5fa
0xUnicorn <ch@cybermonkey.dk>
2021-08-21 00:22:10 +0200

    Converted log format to docker logs

--- monitor.py
+++ monitor.py
@@ -13,23 +13,32 @@ class FtpMonitor:
         with open(self.log_file, 'r') as f:
             while True:
                 line = f.readline()
-                if 'LOGIN' in line:
-                    ftp_login_event = self._create_event(line.split())
+                if line:
+                    sanitized_line = self._sanitize_line(line)
+                    ftp_login_event = self._create_event(sanitized_line)
                     self.dispatcher.dispatch('ftp.login', ftp_login_event)
 
-    def _create_event(self, line: list) -> FtpLoginEvent:
+    def _sanitize_line(self, line: list) -> dict:
+        splitted_line = line.split(' ')
+        if "USER" in line:
+            username = splitted_line[-1]
+        if "530" in line:
+            status = "FAILED"
+        return {
+            "message": line.split(',')[1],
+            "ip": line.split(',')[0].split(' ')[-1],
+            "date": f"{splitted_line[2]}/{splitted_line[1]}/{splitted_line[4]}-{splitted_line[3]}",
+            "username": username,
+            "status": status
+        }
+
+    def _create_event(self, line: dict) -> FtpLoginEvent:
         return FtpLoginEvent(
-            date=self._convert_date([line[2], line[1], line[4], line[3]]),
-            username=line[7].strip('[]'),
-            ip=self._get_ip(line[11]),
-            status=line[8]
+            date=self._convert_date(line['date']),
+            username=line['username'],
+            ip=line['ip'],
+            status=line['status']
         )
 
-    def _convert_date(self, date: list) -> datetime:
-        _full_date = f"{date[0]}/{date[1]}/{date[2]}-{date[3]}"
-        return datetime.strptime(_full_date, '%d/%b/%Y-%H:%M:%S')
-
-    def _get_ip(self, log: str) -> str:
-        log_list = log.split(':')
-        ip = log_list[3][:-1]
-        return ip
+    def _convert_date(self, full_date: list) -> datetime:
+        return datetime.strptime(full_date, '%d/%b/%Y-%H:%M:%S')
